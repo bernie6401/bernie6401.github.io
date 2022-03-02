8bed2e70da49c275f61461bc4ba7d2a6fa876fd9
bernie6401 <48666130+bernie6401@users.noreply.github.com>
2022-03-02 21:00:04 +0800

    modify css file and add blog function

--- blog_implement.js
+++ blog_implement.js
@@ -6,13 +6,24 @@ var list = document.getElementById("list");
 
 btn1.addEventListener("click", function()
 {
-    list.innerHTML = list.innerHTML + 
-    `
-    <div class="article">
-        <h2>${title.value}</h2>
-        <p>${content.value}</P>
-    </div>
-    `;
-    title.value = "";
-    content.value = "";
+    if(title.value != "" && content.value != "")
+    {
+        list.innerHTML = list.innerHTML + 
+        `
+        <div class="article">
+            <br/>
+            <h2>Post Title: ${title.value}</h2>
+            <p>Content: ${content.value}</P>
+            <br/>
+            <hr/>
+        </div>
+        `;
+        title.value = "";
+        content.value = "";
+    }
+
+    else
+    alert('Please enter something!!!');
+
+    console.log(title.value, content.value);
 })
