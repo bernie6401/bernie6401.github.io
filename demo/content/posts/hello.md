1c7d3021625d31c5774f3e7a835ee5e7b6fab083
bernie6401 <bernie6401@gmail.com>
2025-04-02 22:55:27 +0800

    Init Website

---
+++ demo/content/posts/hello.md
@@ -0,0 +1,7 @@
++++
+date = '2025-04-02T22:52:58+08:00'
+draft = false
+title = 'Hello'
++++
+## bbb
+* aaa
---
+++ demo/public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Posts",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Hello",
+    "uri": "/posts/hello/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
