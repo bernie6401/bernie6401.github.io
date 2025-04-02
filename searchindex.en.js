a84a523534052a017c35877fced1a2bced9f0082
bernie6401 <bernie6401@users.noreply.github.com>
2025-04-02 15:09:09 +0000

    deploy: d0c7859c73b05a1c98b2fd589dd84d9dc8d2f4ad

---
+++ searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "SBK Hugo Site \u003e  Posts",
+    "content": "bbb aaa",
+    "description": "bbb aaa",
+    "tags": [],
+    "title": "Hello123",
+    "uri": "/posts/hello/index.html"
+  },
+  {
+    "breadcrumb": "SBK Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "SBK Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "SBK Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "SBK Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
