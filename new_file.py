78e3748e65b2fae7b0e5bb408c6f72010864951a
bernie6401 <bernie6401@gmail.com>
2025-12-20 17:57:53 +0800

    Add book cover and info crawling functions to enhance post generation

--- new_file.py
+++ new_file.py
@@ -2,6 +2,59 @@ import os
 import argparse
 from datetime import datetime
 
+import requests
+
+def crawl_book_cover(bid):
+    # Download book cover image
+        book_img_url = (
+            f"https://www.books.com.tw/img/"
+            f"{bid[0:3]}/{bid[3:6]}/{bid[6:8]}/{bid}.jpg"
+        )
+        book_img = requests.get(book_img_url)
+        if book_img.status_code == 200:
+            img_dir = "assets/posts/"
+            os.makedirs(img_dir, exist_ok=True)
+            img_path = os.path.join(img_dir, f"{os.path.splitext(args.file_path.split('/')[-1])[0]}.jpg")
+            with open(img_path, "wb") as img_file:
+                img_file.write(book_img.content)
+            print(f"✅ 已下載書籍封面至：{img_path}")
+        else:
+            print("❌ 無法下載書籍封面圖片。")
+
+def crawl_book_info(books_id):
+    # 爬蟲書籍資訊
+    import requests
+    from bs4 import BeautifulSoup
+
+    books_url = f"https://www.books.com.tw/products/{books_id}"
+    response = requests.get(books_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        info = {}
+
+        info_block = soup.find("div", class_="type02_p003")
+
+        if info_block:
+            text = ""
+            for li in info_block.find_all("li"):
+                text += li.get_text(strip=True).replace("新功能介紹", "")
+
+
+            text = text.split("語言：")[0]
+            book_info_list = ["出版日期：", "出版社：", "譯者：", "原文作者：", "作者："]
+            info = {"publish_date":"", "publisher":"", "translator":"", "original_author":"", "author":""}
+
+            for item, key in zip(book_info_list, info.keys()):
+                # info
+                if item in text:
+                    info[key] = text.split(item)[-1]
+                    text = text.split(item)[0]
+        print(info)
+    else:
+        print("❌ 無法取得書籍資訊，請確認書籍 ID 是否正確。")
+    
+    return info
+
 def generate_post(file_path):
     # 解析絕對路徑與元件
     file_path = os.path.normpath(file_path)
@@ -22,12 +75,23 @@ def generate_post(file_path):
     # 把 path 轉成 a/b/c 格式 category
     category_str = categories.strip("/")
 
+    if args.books_id:
+        info = crawl_book_info(args.books_id)
+        crawl_book_cover(args.books_id)
+    
     # 產生時間與檔名
     now = datetime.now()
     date_str = now.strftime("%Y-%m-%d")
     date_full = now.strftime("%Y-%m-%d")
     new_filename = f"{date_str}-{name}.md"
-    author_info = '* 作者: \n* 出版社: \n* 出版日期: \n* 譯者: \n* 譯版出版社: \n* 譯版出版日期: \n\n<img src="/assets/posts/" alt="" width="300">\n'
+    if not info.get("translator", ""):
+        author_info = f'* 作者: {info.get("author", "")}\n* 出版社: {info.get("publisher", "")}\n* 出版日期: {info.get("publish_date", "")}\n'
+    elif info.get("original_author", ""):
+        author_info = f'* 作者: {info.get("original_author", "")}\n* 出版社: \n* 出版日期: \n* 譯者: {info.get("translator", "")}\n* 譯版出版社: {info.get("publisher", "")}\n* 譯版出版日期: {info.get("publish_date", "")}\n'
+    else:
+        author_info = f'* 作者: \n* 出版社: \n* 出版日期: \n* 譯者: \n* 譯版出版社: \n* 譯版出版日期: \n'
+
+    author_info += f'\n<img src="/assets/posts/{os.path.splitext(args.file_path.split("/")[-1])[0]}.jpg" alt=""width="300">\n'
 
     # 組成 front matter
     front_matter = f"""---
@@ -51,7 +115,7 @@ comments: true
 
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(front_matter)
-        if "Books Notes" in category_str:
+        if any(cat in category_str for cat in ["Books Notes", "Test"]):
             f.write(f"{author_info}")
         f.write(f"<!-- more -->\n\n")
 
@@ -60,6 +124,7 @@ comments: true
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="建立新的 Jekyll Post")
     parser.add_argument("--file_path", required=True, help="範例：a/b/c/d.md")
+    parser.add_argument("--books_id", nargs="?", help="博客來書城上的書籍 ID（Optional）")
 
     args = parser.parse_args()
     generate_post(args.file_path)
